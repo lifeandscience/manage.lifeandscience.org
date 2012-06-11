9f1aad0ac9984e34855b89b0e2889817fc94064a
Jason Wagner <jcw5002@gmail.com>
2012-06-11 00:33:07 -0400

    Added jQuery UI 1.8.21 so that I can use the Datepicker.

--- www/index.php
+++ www/index.php
@@ -15,7 +15,10 @@
 	<title>Events Management</title>
 	<meta charset="utf-8" /> 
 	<link rel="stylesheet" href="/css/styles.css" type="text/css" />
-	<script src="/js/jquery-1.7.2.min.js"></script>	
+	<link rel="stylesheet" href="/css/ui-lightness/jquery-ui-1.8.21.custom.css" type="text/css" />
+	<script src="/js/jquery-1.7.2.min.js"></script>
+	<script src="/js/jquery-ui-1.8.21.custom.min.js"></script>	
+
 	<!--[if IE]>
 	  <script type="text/javascript" src="/js/html5.js"></script>
 	<![endif]-->
@@ -30,7 +33,7 @@
 				<ul class="topnav"> 
 					<li><a href="/">Home</a></li>				
 					<li><a href="/events/add">Add Weekly Event</a></li>
-					<li><a href="/events/add">Add Special Event</a></li>
+					<li><a href="/events/addspecial">Add Special Event</a></li>
 					<li><a href="/events/edit/1">Edit Event</a></li>
 					<li><a href="/events/manage">Manage Events</a></li>					
 										
