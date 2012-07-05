27fb2565549009b65c682c8bc96af167908cbe09
Ben Schell <ben@benschell.org>
2012-07-05 13:34:33 -0400

    Some changes to help with deployment, allowing for a config file to reside in the top level of the directory structure, but not be included in the repository.

--- www/bin/config.inc.php
+++ www/bin/config.inc.php
@@ -1,5 +1,7 @@
 <?php
-
+if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/../config.inc.php')){
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/../config.inc.php');
+}else{
 	//database settings
 	define('SITE_DB_NAME', 'ncmls');
 	define('SITE_DB_USER', 'root');
@@ -7,6 +9,6 @@
 	define('SITE_DB_HOST', 'localhost');
 	define('DB_CHARSET', 'utf8');
 	define('DB_COLLATE', '');
-
+}
 ?>
 
